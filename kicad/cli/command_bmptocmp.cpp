f1d6f979430e1802771ed2bf0dd778792e1e6fd1
Daniel Liu <mr.picklepinosaur@gmail.com>
2025-03-30 01:42:01 +0100

    wip impl bmp2cmp

--- kicad/cli/command_bmptocmp.cpp
+++ kicad/cli/command_bmptocmp.cpp
@@ -29,12 +29,107 @@
 #include <macros.h>
 #include <wx/tokenzr.h>
 
+#include <bitmap2cmp_frame.h>
+#include <bitmap2component.h>
+#include <bitmap2cmp_panel.h>
+#include <bitmap2cmp_settings.h>
+#include <potracelib.h>
+#include <bitmap.h>
+//#include <bitmap_io.h>
+
+#define ARG_OUTPUT_FORMAT "--output-format"
+#define ARG_DPI_X "--dpi-x"
+#define ARG_DPI_Y "--dpi-y"
+#define ARG_OUTPUT_LAYER "--output-layer"
+#define ARG_OUTPUT "--output"
+
 CLI::BMP_TO_CMP_COMMAND::BMP_TO_CMP_COMMAND() : COMMAND( "bmp2cmp" )
 {
     m_argParser.add_description( UTF8STDSTR( _( "Convert bitmap images to component footprints" ) ) );
+
+    addCommonArgs( true, false, false, false );
+
+    m_argParser.add_argument( "-o", ARG_OUTPUT )
+            .default_value( std::string() )
+            .help( UTF8STDSTR( _( "The file to output the converted component to" ) ) );
+
+    m_argParser.add_argument( "--of", ARG_OUTPUT_FORMAT )
+            .default_value( std::string() )
+            .help( UTF8STDSTR( _( "The format to output the bitmap to" ) ) );
+
+    m_argParser.add_argument( "--dx", ARG_DPI_X )
+            .help( UTF8STDSTR( _( "The X DPI of the image" ) ) )
+            .scan<'i', int>()
+            .default_value(300);
+
+    m_argParser.add_argument( "--dy", ARG_DPI_Y )
+            .help( UTF8STDSTR( _( "The Y DPI of the image" ) ) )
+            .scan<'i', int>()
+            .default_value(300);
+
+    m_argParser.add_argument( "--ol", ARG_OUTPUT_LAYER )
+            .default_value( std::string() )
+            .help( UTF8STDSTR( _( "The layer the resulting footprint is on" ) ) );
 }
 
 int CLI::BMP_TO_CMP_COMMAND::doPerform( KIWAY& aKiway )
 {
+    // cli arguments
+
+    // std::string format = m_argParser.get<std::string>( ARG_OUTPUT_FORMAT );
+    // TODO hardcoded to footprint only for now
+    OUTPUT_FMT_ID aFormat = FOOTPRINT_FMT;
+
+    /*
+    if( aFormat == FOOTPRINT_FMT )
+    {
+        switch( m_layerCtrl->GetSelection() )
+        {
+        case 0: layer = wxT( "F.Cu" );      break;
+        case 1: layer = wxT( "F.SilkS" );   break;
+        case 2: layer = wxT( "F.Mask" );    break;
+        case 3: layer = wxT( "Dwgs.User" ); break;
+        case 4: layer = wxT( "Cmts.User" ); break;
+        case 5: layer = wxT( "Eco1.User" ); break;
+        case 6: layer = wxT( "Eco2.User" ); break;
+        case 7: layer = wxT( "F.Fab" );     break;
+        }
+    }
+    */
+    int a_Dpi_X = m_argParser.get<int>(ARG_DPI_X);
+    int a_Dpi_Y = m_argParser.get<int>(ARG_DPI_Y);
+    const wxString aLayer = From_UTF8(m_argParser.get<std::string>(ARG_OUTPUT_LAYER).c_str());
+
+    // potrace_bitmap_t* potrace_bitmap = bm_new( m_NB_Image.GetWidth(), m_NB_Image.GetHeight() );
+    potrace_bitmap_t* potrace_bitmap = bm_new( 102.4, 102.4 );
+    if( !potrace_bitmap )
+    {
+        // TODO error message
+        wxFprintf( stderr, _( "Failed to create bitmap image\n" ) );
+        return 1;
+    }
+
+#if 0
+    /* fill the bitmap with data */
+    for( int y = 0; y < m_NB_Image.GetHeight(); y++ )
+    {
+        for( int x = 0; x < m_NB_Image.GetWidth(); x++ )
+        {
+            unsigned char pixel = m_NB_Image.GetGreen( x, y );
+            BM_PUT( potrace_bitmap, x, y, pixel ? 0 : 1 );
+        }
+    }
+#endif
+
+    WX_STRING_REPORTER reporter;
+    std::string aData = m_argParser.get<std::string>(ARG_OUTPUT);
+    BITMAPCONV_INFO    converter( aData, reporter );
+
+    converter.ConvertBitmap( potrace_bitmap, aFormat, a_Dpi_X, a_Dpi_Y, aLayer );
+
+    if( reporter.HasMessage() ) {
+
+    }
+
     return 0;
 }
